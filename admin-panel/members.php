d6c97a21e8b6c1208db7e3343d3c7e291ddacef7
elwan7dev <ahmed.elwan568@gmail.com>
2020-04-17 08:23:45 +0200

    design Edit spliting page

--- admin-panel/members.php
+++ admin-panel/members.php
@@ -12,6 +12,7 @@ if (isset($_SESSION['username'])) {
 
     // split page with GET request
     $action = isset($_GET['action']) ? $_GET['action'] : 'manage';
+
     switch ($action) {
         case 'manage':
             echo 'Welcome in manage page';
@@ -19,10 +20,51 @@ if (isset($_SESSION['username'])) {
         case 'add':
             echo 'Welcome in add page';
             break;
-        case 'edit':
-            echo 'Welcome in edit page - your id = '. $_SESSION['userid'];
-            
-            break;
+        case 'edit': // Edit page
+            echo 'Welcome in edit page - your id = ' . $_SESSION['userid'];?>
+
+<!-- start html componants -->
+<h1 class="text-center">Edit Member</h1>
+<div class="container">
+    <form>
+        <!-- start username field -->
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="username">Username</label>
+            <div class="col-sm-10">
+                <input type="text" name="username" class="form-control form-control-lg" autocomplete="off">
+            </div>
+        </div>
+        <!-- start Password field -->
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="username">Password</label>
+            <div class="col-sm-10">
+                <input type="password" name="password" class="form-control form-control-lg" autocomplete="off">
+            </div>
+        </div>
+        <!-- start email field -->
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="username">Email</label>
+            <div class="col-sm-10">
+                <input type="email" name="email" class="form-control form-control-lg">
+            </div>
+        </div>
+        <!-- start Full Name field -->
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="username">Full Name</label>
+            <div class="col-sm-10">
+                <input type="text" name="fullname" class="form-control form-control-lg">
+            </div>
+        </div>
+        <!-- start username field -->
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <input type="button" value="save" class="btn btn-primary">
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php break;
         case 'stats':
             echo 'Welcome in stats page';
             break;
@@ -33,7 +75,6 @@ if (isset($_SESSION['username'])) {
             echo 'Error 404 : This page not found';
             break;
     }
-    
 
     // footer temp
     include $tpl . 'footer.php';
