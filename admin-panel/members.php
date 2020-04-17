76c14084f63649c8607406d03758056a37d2b842
elwan7dev <ahmed.elwan568@gmail.com>
2020-04-17 21:37:20 +0200

    code member-edit splitling page

--- admin-panel/members.php
+++ admin-panel/members.php
@@ -20,51 +20,56 @@ if (isset($_SESSION['username'])) {
         case 'add':
             echo 'Welcome in add page';
             break;
-        case 'edit': // Edit page
-            echo 'Welcome in edit page - your id = ' . $_SESSION['userid'];?>
+        case 'edit': // ***************** Member Edit page *******************
+            // check if get request user id is numeric & get the integer value of it.
+            $userid = (isset($_GET['userid']) && is_numeric($_GET['userid'])) ? intval($_GET['userid']) : 0;
+
+            // Select all fields from record depend on this id
+            $stmt = $conn->prepare("SELECT * FROM users WHERE userid=? LIMIT 1");
+            //execute Query
+            $stmt->execute(array($userid)); //if userid are equal- select
+            $row = $stmt->fetch(); //fetch row data from DB
+
+            // if there is such ID - show the form
+            if ($stmt->rowCount() > 0) {?>
 
 <!-- start html componants -->
 <h1 class="text-center">Edit Member</h1>
-<div class="container">
+<div class="container" style="width: 70%;">
     <form>
-        <!-- start username field -->
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="username">Username</label>
-            <div class="col-sm-10">
-                <input type="text" name="username" class="form-control form-control-lg" autocomplete="off">
+        <!-- start username & password in same row field -->
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="username">Username</label>
+                <input type="text" name="username" class="form-control" value=<?php echo $row['Username']; ?> autocomplete="off">
             </div>
-        </div>
-        <!-- start Password field -->
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="username">Password</label>
-            <div class="col-sm-10">
-                <input type="password" name="password" class="form-control form-control-lg" autocomplete="off">
+            <div class="form-group col-md-6">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" autocomplete="off">
             </div>
         </div>
         <!-- start email field -->
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="username">Email</label>
-            <div class="col-sm-10">
-                <input type="email" name="email" class="form-control form-control-lg">
-            </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" class="form-control" value=<?php echo $row['Email']; ?>>
         </div>
         <!-- start Full Name field -->
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="username">Full Name</label>
-            <div class="col-sm-10">
-                <input type="text" name="fullname" class="form-control form-control-lg">
-            </div>
+        <div class="form-group">
+            <label for="fullname">Full Name</label>
+            <input type="text" name="fullname" class="form-control" value=<?php echo $row['FullName']; ?>>
         </div>
-        <!-- start username field -->
-        <div class="form-group row">
-            <div class="col-sm-10">
-                <input type="button" value="save" class="btn btn-primary">
-            </div>
-        </div>
+        <!-- start button field -->
+        <input type="button" value="save" class="btn btn-primary">
+        
+        
     </form>
 </div>
 
-<?php break;
+<?php
+        } else {
+                echo "<h1>Error:No such ID</h1>";
+            }
+            break;
         case 'stats':
             echo 'Welcome in stats page';
             break;
