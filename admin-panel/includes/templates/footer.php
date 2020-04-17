24a2e574ecec47a7f9500853571260bb2f0e7e29
elwan7dev <ahmed.elwan568@gmail.com>
2020-04-17 20:52:54 +0200

    jquery-3.5.0 cause errors in toggler button so, I back to jquery-3.4.1

--- admin-panel/includes/templates/footer.php
+++ admin-panel/includes/templates/footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
 
 </div>
-<!-- JS - jquery-3.5.0 -->
-<script src="<?php echo $js ?>jquery-3.5.0.min.js"></script>
+<!-- JS - jquery-3.5.0  cause errors in toggler button so, I back to jquery-3.4.1 -->
+<script src="<?php echo $js ?>jquery-3.4.1.min.js"></script>
 <!-- JS - bootstrap -->
 <script src="<?php echo $js ?>bootstrap.min.js"></script>
 <!-- JS - font awesome -->
