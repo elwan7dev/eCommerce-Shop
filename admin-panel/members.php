1100bf887ee256c774b7883c13d08fd00694df44
elwan7dev <ahmed.elwan568@gmail.com>
2020-04-18 04:22:59 +0200

    code member-update splitting page

--- admin-panel/members.php
+++ admin-panel/members.php
@@ -20,7 +20,7 @@ if (isset($_SESSION['username'])) {
         case 'add':
             echo 'Welcome in add page';
             break;
-        case 'edit': // ***************** Member Edit page *******************
+        case 'edit': // ************* Start Member Edit page *******************
             // check if get request user id is numeric & get the integer value of it.
             $userid = (isset($_GET['userid']) && is_numeric($_GET['userid'])) ? intval($_GET['userid']) : 0;
 
@@ -36,43 +36,102 @@ if (isset($_SESSION['username'])) {
 <!-- start html componants -->
 <h1 class="text-center">Edit Member</h1>
 <div class="container" style="width: 70%;">
-    <form>
+    <form action="?action=update" method="POST">
+        <!-- add userid input here to post it in form add get it in update page   -->
+        <input type="hidden" name="userid" value="<?php echo $userid; ?>">
+
         <!-- start username & password in same row field -->
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="username">Username</label>
-                <input type="text" name="username" class="form-control" value=<?php echo $row['Username']; ?> autocomplete="off">
+                <input type="text" name="username" class="form-control" value="<?php echo $row['Username']; ?>"
+                    autocomplete="off">
             </div>
             <div class="form-group col-md-6">
                 <label for="password">Password</label>
-                <input type="password" name="password" class="form-control" autocomplete="off">
+                <input type="hidden" name="oldPassword" value="<?php echo $row['Password']; ?>">
+                <input type="password" name="newPassword" class="form-control" autocomplete="off">
             </div>
         </div>
         <!-- start email field -->
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" name="email" class="form-control" value=<?php echo $row['Email']; ?>>
+            <input type="email" name="email" class="form-control" value="<?php echo $row['Email']; ?>">
         </div>
         <!-- start Full Name field -->
         <div class="form-group">
             <label for="fullname">Full Name</label>
-            <input type="text" name="fullname" class="form-control" value=<?php echo $row['FullName']; ?>>
+            <input type="text" name="fullname" class="form-control" value="<?php echo $row['FullName']; ?>">
+
         </div>
         <!-- start button field -->
-        <input type="button" value="save" class="btn btn-primary">
-        
-        
+        <input type="submit" value="save" class="btn btn-primary">
+
+
     </form>
 </div>
 
 <?php
-        } else {
-                echo "<h1>Error:No such ID</h1>";
+} else {
+                // Error:No such ID
+                echo "  <div class='container' style='width: 70%; margin-top: 50px;'>
+                            <div class='alert alert-danger' role='alert'>
+                                Error: No Such ID
+                            </div>
+                         </div>";
             }
-            break;
-        case 'stats':
-            echo 'Welcome in stats page';
-            break;
+            break; // *************** End Member Edit page *****************
+
+        case 'update': // ************* Start Member Update page ***************
+            echo "<h1 class='text-center'>Update Member</h1>";
+            // check if user coming from http POST request to prevent browseing page directly
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+                // get the vars from the form
+                $userId = $_POST['userid'];
+                $userName = $_POST['username'];
+                $email = $_POST['email'];
+                $fullName = $_POST['fullname'];
+               
+
+                // password trick
+                $pass = empty($_POST['newPassword']) ? $_POST['oldPassword'] : sha1($_POST['newPassword']);
+               
+                // Update the DB record with this info 
+                $stmt = $conn->prepare("UPDATE users SET username = ?, email = ?, fullname = ?, password = ?
+                                        WHERE userid =?");
+                                        //if i used $_SESSION['userid'] instead if $_GET['userid'] = $userId
+                                        //fatal error update in current user only
+                $stmt->execute(array($userName, $email, $fullName, $pass, $userId)); 
+                $count = $stmt->rowCount();
+
+                if ($count > 0) {
+                    // Successful Message
+                    // echo "[ $count ]" . ' Record Updated';
+                    echo "  <div class='container' style='width: 70%;'>
+                                <div class='alert alert-success' role='alert'>
+                                    <strong>$count</strong>  Record Have Been Updated
+                                </div>
+                            </div>";
+                } else {
+                    // Error Message No Data Updated Yet!
+                    echo "  <div class='container' style='width: 70%;'>
+                                <div class='alert alert-warning' role='alert'>
+                                    No Data Updated Yet!
+                                </div>
+                            </div>";
+                }
+
+            } else {
+                // Error: You Can't Browse This Page Directly
+                echo "  <div class='container' style='width: 70%;'>
+                                <div class='alert alert-danger' role='alert'>
+                                Error: You Can't Browse This Page Directly
+                                </div>
+                            </div>";
+            }
+
+            break; // ************* Start Member Update page ***************
         case 'delete':
             echo 'Welcome in delete page';
             break;
