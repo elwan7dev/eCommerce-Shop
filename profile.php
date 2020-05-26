db2fd8176a41a450a012f77ca66aef6739d90167
elwan7dev <ahmed.elwan568@gmail.com>
2020-05-26 16:13:06 +0200

    design profile page

--- profile.php
+++ profile.php
@@ -14,98 +14,102 @@ if (isset($_SESSION['username'])) { ?>
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    <h2>Profile</h2>
+                    <h1>Profile</h1>
                 </div>
                 <!-- breadcrumb -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="/eCommerce">Home</a></li>
                         <li class="breadcrumb-item active">User Profile</li>
                     </ol>
                 </div>
             </div>
-        </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.container -->
     </section>
     <!-- /.content-header -->
 
     <!-- Main content -->
     <section class="content">
         <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <!-- Profile Image -->
-                    <div class="card card-primary card-outline">
-                        <div class="card-body box-profile">
-                            <div class="text-center">
-                                <img class="profile-user-img img-fluid img-circle"
-                                    src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+            <div class="row info">
+                <!-- <div class="info"> -->
+                    <div class="col-md-3">
+                        <!-- Profile Image -->
+                        <div class="card card-primary card-outline">
+                            <div class="card-body box-profile">
+                                <div class="text-center">
+                                    <img class="profile-user-img img-fluid img-circle" src="layout/images/avatar.jpg"
+                                        alt="User profile picture">
+                                </div>
+
+                                <h3 class="profile-username text-center">Nina Mcintire</h3>
+
+                                <p class="text-muted text-center">Software Engineer</p>
+
+                                <ul class="list-group list-group-unbordered mb-3">
+                                    <li class="list-group-item">
+                                        <b>Followers</b> <a class="float-right">1,322</a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <b>Following</b> <a class="float-right">543</a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <b>Friends</b> <a class="float-right">13,287</a>
+                                    </li>
+                                </ul>
+
+                                <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
                             </div>
-
-                            <h3 class="profile-username text-center">Nina Mcintire</h3>
-
-                            <p class="text-muted text-center">Software Engineer</p>
-
-                            <ul class="list-group list-group-unbordered mb-3">
-                                <li class="list-group-item">
-                                    <b>Followers</b> <a class="float-right">1,322</a>
-                                </li>
-                                <li class="list-group-item">
-                                    <b>Following</b> <a class="float-right">543</a>
-                                </li>
-                                <li class="list-group-item">
-                                    <b>Friends</b> <a class="float-right">13,287</a>
-                                </li>
-                            </ul>
-
-                            <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                            <!-- /.card-body -->
                         </div>
-                        <!-- /.card-body -->
+                        <!-- /.card -->
+
+                        <!-- About Me Box -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">About Me</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <strong><i class="fas fa-book mr-1"></i> Education</strong>
+
+                                <p class="text-muted">
+                                    B.S. in Computer Science from the University of Tennessee at Knoxville
+                                </p>
+
+                                <hr>
+
+                                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+                                <p class="text-muted">Malibu, California</p>
+
+                                <hr>
+
+                                <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+
+                                <p class="text-muted">
+                                    <span class="tag tag-danger">UI Design</span>
+                                    <span class="tag tag-success">Coding</span>
+                                    <span class="tag tag-info">Javascript</span>
+                                    <span class="tag tag-warning">PHP</span>
+                                    <span class="tag tag-primary">Node.js</span>
+                                </p>
+
+                                <hr>
+
+                                <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+
+                                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                                    fermentum enim neque.</p>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
                     </div>
-                    <!-- /.card -->
-
-                    <!-- About Me Box -->
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h3 class="card-title">About Me</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <strong><i class="fas fa-book mr-1"></i> Education</strong>
-
-                            <p class="text-muted">
-                                B.S. in Computer Science from the University of Tennessee at Knoxville
-                            </p>
-
-                            <hr>
-
-                            <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
-
-                            <p class="text-muted">Malibu, California</p>
-
-                            <hr>
-
-                            <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
-
-                            <p class="text-muted">
-                                <span class="tag tag-danger">UI Design</span>
-                                <span class="tag tag-success">Coding</span>
-                                <span class="tag tag-info">Javascript</span>
-                                <span class="tag tag-warning">PHP</span>
-                                <span class="tag tag-primary">Node.js</span>
-                            </p>
-
-                            <hr>
-
-                            <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
-
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                                fermentum enim neque.</p>
-                        </div>
-                        <!-- /.card-body -->
-                    </div>
-                    <!-- /.card -->
-                </div>
-                <!-- /.col -->
+                    <!-- /.col -->
+                <!-- </div> -->
+                
 
                 <div class="col-md-9">
                     <div class="card">
@@ -126,7 +130,7 @@ if (isset($_SESSION['username'])) { ?>
                                     <div class="post">
                                         <div class="user-block">
                                             <img class="img-circle img-bordered-sm"
-                                                src="../../dist/img/user1-128x128.jpg" alt="user image">
+                                                src="layout/images/avatar.jpg" alt="user image">
                                             <span class="username">
                                                 <a href="#">Jonathan Burke Jr.</a>
                                                 <a href="#" class="float-right btn-tool"><i
@@ -164,7 +168,7 @@ if (isset($_SESSION['username'])) { ?>
                                     <div class="post clearfix">
                                         <div class="user-block">
                                             <img class="img-circle img-bordered-sm"
-                                                src="../../dist/img/user7-128x128.jpg" alt="User Image">
+                                                src="layout/images/avatar.jpg" alt="User Image">
                                             <span class="username">
                                                 <a href="#">Sarah Ross</a>
                                                 <a href="#" class="float-right btn-tool"><i
@@ -196,7 +200,7 @@ if (isset($_SESSION['username'])) { ?>
                                     <div class="post">
                                         <div class="user-block">
                                             <img class="img-circle img-bordered-sm"
-                                                src="../../dist/img/user6-128x128.jpg" alt="User Image">
+                                                src="layout/images/avatar.jpg" alt="User Image">
                                             <span class="username">
                                                 <a href="#">Adam Jones</a>
                                                 <a href="#" class="float-right btn-tool"><i
@@ -207,22 +211,22 @@ if (isset($_SESSION['username'])) { ?>
                                         <!-- /.user-block -->
                                         <div class="row mb-3">
                                             <div class="col-sm-6">
-                                                <img class="img-fluid" src="../../dist/img/photo1.png" alt="Photo">
+                                                <img class="img-fluid" src="layout/images/photo1.png" alt="Photo">
                                             </div>
                                             <!-- /.col -->
                                             <div class="col-sm-6">
                                                 <div class="row">
                                                     <div class="col-sm-6">
-                                                        <img class="img-fluid mb-3" src="../../dist/img/photo2.png"
+                                                        <img class="img-fluid mb-3" src="layout/images/photo2.png"
                                                             alt="Photo">
-                                                        <img class="img-fluid" src="../../dist/img/photo3.jpg"
+                                                        <img class="img-fluid" src="layout/images/photo3.jpg"
                                                             alt="Photo">
                                                     </div>
                                                     <!-- /.col -->
                                                     <div class="col-sm-6">
-                                                        <img class="img-fluid mb-3" src="../../dist/img/photo4.jpg"
+                                                        <img class="img-fluid mb-3" src="layout/images/photo4.jpg"
                                                             alt="Photo">
-                                                        <img class="img-fluid" src="../../dist/img/photo1.png"
+                                                        <img class="img-fluid" src="layout/images/photo1.png"
                                                             alt="Photo">
                                                     </div>
                                                     <!-- /.col -->
