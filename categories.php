1f75d4a8d52bd9149529144f70eecd145cd1234f
elwan7dev <ahmed.elwan568@gmail.com>
2020-05-30 17:56:48 +0200

    Add categories items number in header, add breadcrumb, restructure page

--- categories.php
+++ categories.php
@@ -8,39 +8,67 @@ $pageTitle = "Categories | $catName";
 include 'init.php'; 
 
 $items = getItems('cat_id' , $catId);
+// Numbers of items in specific category
+$catItems = countItems('item_id', 'items', "WHERE cat_id="."$catId"." " );
+
 ?>
-
-<div class="container">
-    <h1 class ="text-center"> <?php echo $catName  ?> </h1>
-    <div class="ads">
-        <div class="row">
-        
-            <?php
-                if (! empty($items)) {
-                    foreach ($items as $item) { ?>
-                        <div class="col-sm-6 col-md-3">
-                            <div class="card">
-                                <img src="layout/images/image.jpg" class="card-img-top" alt="AD Image">
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo $item['name'] ?> </h5>
-                                    <p class="card-text"><?php echo $item['price'] ?> </p>
-                                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                                </div>
-                            </div>
-                        </div>
-                    <?php
-                    }
-                }else {
-                    echo "No Items Found in this cat";
-                }
-
-            
-            ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+     <!-- Content Header (Page header) -->
+     <section class="content-header cat-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1><?php echo $catName  ?> </h1> 
+                    <span class="badge badge-primary align-middle">( <?php echo $catItems ?> Items Found )</span>
+                </div>
+                <!-- breadcrumb -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/eCommerce">Home</a></li>
+                        <li class="breadcrumb-item active"><?php echo $catName  ?></li>
+                    </ol>
+                </div>
+            </div>
         </div>
-    </div>
-    
-   
+        <!-- /.container -->
+    </section>
+    <!-- /.content-header -->
 
+    <!-- Main content -->
+    <section class="content">
+        <div class="container">
+            <div class="ads">
+                <div class="row">
+                    <?php
+                        if (! empty($items)) {
+                            foreach ($items as $item) { ?>
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="card item-box">
+                                        <span class="price-tag "><?php echo '$'. $item['price']; ?></span>
+                                        <img src="layout/images/image.jpg" class="card-img-top" alt="AD Image">
+                                        <div class="card-body">
+                                            <h5 class="card-title"><?php echo $item['name']; ?> </h5>
+                                            <p class="card-text"><?php echo $item['description']; ?> </p>
+                                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
+                            }
+                        }else {
+                            echo "No Items Found in this cat";
+                        }
+
+                    
+                    ?>
+                </div>
+            </div>
+            
+        
+
+        </div>
+    </section>
 </div>
 
 
--- profile.php
+++ profile.php
@@ -145,11 +145,12 @@ if (isset($_SESSION['username']) || isset($_SESSION['admin'])) {
                                             echo '<div class="row">';
                                             foreach ($items as $item) { ?>
                                             <div class="col-sm-6 col-md-3">
-                                                <div class="card">
+                                                <div class="card item-box">
+                                                    <span class="price-tag"><?php echo '$' . $item['price'];?></span>
                                                     <img src="layout/images/image.jpg" class="card-img-top" alt="AD Image">
                                                     <div class="card-body">
-                                                        <h5 class="card-title"><?php echo $item['name'] ?> </h5>
-                                                        <p class="card-text"><?php echo $item['price'] ?> </p>
+                                                        <h5 class="card-title"><?php echo $item['name']; ?> </h5>
+                                                        <p class="card-text"><?php echo $item['description']; ?> </p>
                                                         <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
                                                     </div>
                                                 </div>
